1f3af9b9d0d870260e31f816c232b7d6c0a47484
Toni Crottet <toni@azoom.ch>
2016-01-29 13:45:19 +0100

    no message

---
+++ prod-site/core/config/config.inc.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *  MODX Configuration file
+ */
+$database_type = 'mysql';
+$database_server = 'db12.exigo.ch';
+$database_user = 'cms-db-shv';
+$database_password = 'shv20xy%15';
+$database_connection_charset = 'utf8';
+$dbase = 'cms-db';
+$table_prefix = 'modx_';
+$database_dsn = 'mysql:host=db12.exigo.ch;dbname=cms-db;charset=utf8';
+$config_options = array (
+);
+$driver_options = array (
+);
+
+$lastInstallTime = 1423586270;
+
+$site_id = 'modx54da33deb58956.39829118';
+$site_sessionname = 'SN54da3333d3c8f';
+$https_port = '443';
+$uuid = 'a341d00d-8894-4641-bd97-fc82b5bcc715';
+
+if (!defined('MODX_CORE_PATH')) {
+    $modx_core_path= '/home/web/1596921423504662488150/htdocs/core/';
+    define('MODX_CORE_PATH', $modx_core_path);
+}
+if (!defined('MODX_PROCESSORS_PATH')) {
+    $modx_processors_path= '/home/web/1596921423504662488150/htdocs/core/model/modx/processors/';
+    define('MODX_PROCESSORS_PATH', $modx_processors_path);
+}
+if (!defined('MODX_CONNECTORS_PATH')) {
+    $modx_connectors_path= '/home/web/1596921423504662488150/htdocs/connectors/';
+    $modx_connectors_url= '/connectors/';
+    define('MODX_CONNECTORS_PATH', $modx_connectors_path);
+    define('MODX_CONNECTORS_URL', $modx_connectors_url);
+}
+if (!defined('MODX_MANAGER_PATH')) {
+    $modx_manager_path= '/home/web/1596921423504662488150/htdocs/manager/';
+    $modx_manager_url= '/manager/';
+    define('MODX_MANAGER_PATH', $modx_manager_path);
+    define('MODX_MANAGER_URL', $modx_manager_url);
+}
+if (!defined('MODX_BASE_PATH')) {
+    $modx_base_path= '/home/web/1596921423504662488150/htdocs/';
+    $modx_base_url= '/';
+    define('MODX_BASE_PATH', $modx_base_path);
+    define('MODX_BASE_URL', $modx_base_url);
+}
+if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+    $isSecureRequest = false;
+} else {
+    $isSecureRequest = ((isset ($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on') || $_SERVER['SERVER_PORT'] == $https_port);
+}
+if (!defined('MODX_URL_SCHEME')) {
+    $url_scheme=  $isSecureRequest ? 'https://' : 'http://';
+    define('MODX_URL_SCHEME', $url_scheme);
+}
+if (!defined('MODX_HTTP_HOST')) {
+    if(defined('PHP_SAPI') && (PHP_SAPI == "cli" || PHP_SAPI == "embed")) {
+        $http_host='www.meteo-shv.ch';
+        define('MODX_HTTP_HOST', $http_host);
+    } else {
+        $http_host= array_key_exists('HTTP_HOST', $_SERVER) ? $_SERVER['HTTP_HOST'] : 'www.meteo-shv.ch';
+        if ($_SERVER['SERVER_PORT'] != 80) {
+            $http_host= str_replace(':' . $_SERVER['SERVER_PORT'], '', $http_host); // remove port from HTTP_HOST
+        }
+        $http_host .= ($_SERVER['SERVER_PORT'] == 80 || $isSecureRequest) ? '' : ':' . $_SERVER['SERVER_PORT'];
+        define('MODX_HTTP_HOST', $http_host);
+    }
+}
+if (!defined('MODX_SITE_URL')) {
+    $site_url= $url_scheme . $http_host . MODX_BASE_URL;
+    define('MODX_SITE_URL', $site_url);
+}
+if (!defined('MODX_ASSETS_PATH')) {
+    $modx_assets_path= '/home/web/1596921423504662488150/htdocs/assets/';
+    $modx_assets_url= '/assets/';
+    define('MODX_ASSETS_PATH', $modx_assets_path);
+    define('MODX_ASSETS_URL', $modx_assets_url);
+}
+if (!defined('MODX_LOG_LEVEL_FATAL')) {
+    define('MODX_LOG_LEVEL_FATAL', 0);
+    define('MODX_LOG_LEVEL_ERROR', 1);
+    define('MODX_LOG_LEVEL_WARN', 2);
+    define('MODX_LOG_LEVEL_INFO', 3);
+    define('MODX_LOG_LEVEL_DEBUG', 4);
+}
+if (!defined('MODX_CACHE_DISABLED')) {
+    $modx_cache_disabled= false;
+    define('MODX_CACHE_DISABLED', $modx_cache_disabled);
+}
